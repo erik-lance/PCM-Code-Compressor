b336b73038dd2511874d8f4fe596abd5339c90ec
erik-lance <eriklance@gmail.com>
2023-03-28 00:23:55 +0800

    Added error and continue sections

--- tionEmp2.asm
+++ tionEmp2.asm
@@ -8,7 +8,32 @@ main:
     
     ; 89 Y
     ; 78 N
+
+input:
+    PRINT_STRING "Input 12-bit code: "
     
-    
+
+tapos:
     xor eax, eax
     ret
+
+error_length:
+    PRINT_STRING "Input should be 12 bits in length!"
+    jmp continue
+
+error_binary:
+    PRINT_STRING "Input should be 1's and 0's only!"
+
+continue:
+    NEWLINE
+    PRINT_STRING "Do you want to continue (Y/N)?"
+    GET_UDEC 1, DL
+    cmp DL, 89      ; Check if yes
+    je input
+    cmp DL, 78      ; Check if no
+    je tapos
+    
+    PRINT_STRING "Error, invalid input"
+    jmp continue
+
+    
